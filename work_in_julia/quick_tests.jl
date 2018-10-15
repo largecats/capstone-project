e0ada1398ca689fc6cbe1f5af4de4580f29a7d70
Linfan XIAO (Linda) <linfan.xiao@u.yale-nus.edu.sg>
2018-10-15 17:30:22 +0800

    New issue identified
    rank() throws weird "InExactError()" when taking some complex matrices

--- work_in_julia/quick_tests.jl
+++ work_in_julia/quick_tests.jl
@@ -48,3 +48,28 @@ evaluate_xi(L, 1, xi)
 U = VectorBoundaryForm([1 2; 3 4], [4 3; 2 1])
 get_boundaryCondition(L, U, symXi)
 get_boundaryCondition(L, U, xi)
+
+# Complex entries
+t = symbols("t")
+(a,b) = (0,1)
+symPFunctions = [t+im t*im t]
+pFunctions = [t->t+im t->t*im t->t]
+symL = SymLinearDifferentialOperator(symPFunctions, (a,b), t)
+L = LinearDifferentialOperator(pFunctions, (a,b), symL)
+n = 2
+MCand = rand(Uniform(1.0,10.0), n, n)
+NCand = rand(Uniform(1.0,10.0), n, n)
+U = VectorBoundaryForm(MCand, NCand)
+pDerivMatrix = [t->t+im t->t; t->t*im t->im]
+construct_validAdjoint(L, U, pDerivMatrix)
+B = get_B(L; pDerivMatrix = pDerivMatrix)
+BHat = get_BHat(L, B)
+Uc = get_Uc(U)
+H = get_H(U, Uc)
+J = get_J(BHat, H)
+adjointU = get_adjoint(J)
+n = convert(Int, size(J)[1]/2)
+Pstar = J[(n+1):2n,1:n]
+Qstar = J[(n+1):2n, (n+1):2n]
+adjoint = VectorBoundaryForm(Pstar, Qstar)
+rank(hcat(Pstar, Qstar))
